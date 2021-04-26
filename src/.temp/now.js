6233be9d746837c397fd0c32d9ea3b3bfc3ee072
liminalfun <xelanawotka@gmail.com>
2021-04-26 17:02:31 -0400

    Resolve npm + git-gateway errors

--- src/.temp/now.js
+++ src/.temp/now.js
@@ -1,1 +1,1 @@
-export default 1619468665417
+export default 1619470805687
--- src/.temp/plugins-server.js
+++ src/.temp/plugins-server.js
@@ -1,3 +1,8 @@
+import plugin_gridsome_plugin_netlify_cms_6 from "/Users/xela/Sites/h9-test/node_modules/gridsome-plugin-netlify-cms/gridsome.client.js"
 
 export default [
+  {
+    run: plugin_gridsome_plugin_netlify_cms_6,
+    options: {"publicPath":"/admin","htmlTitle":"Content Editor","configPath":"src/admin/config.yml","modulePath":"/Users/xela/Sites/h9-test/node_modules/gridsome-plugin-netlify-cms/lib/cms.js","htmlPath":"/Users/xela/Sites/h9-test/node_modules/gridsome-plugin-netlify-cms/templates/index.html","injectScript":true,"enableIdentityWidget":true,"debug":false}
+  }
 ]
