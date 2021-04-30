86758666dc3ac6e23af46b813eee9931502a6900
liminalfun <xelanawotka@gmail.com>
2021-04-30 19:46:18 -0400

    Update config for scss

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,8 +4,28 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const path = require('path')
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/*.scss'),
+      ],
+    })
+}
+
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'house9 test',
+  chainWebpack (config) {
+    // Load variables for all vue-files
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+
+    types.forEach(type => {
+      addStyleResource(config.module.rule('scss').oneOf(type))
+    })
+  },
   transformers: {
     remark: {
       externalLinksTarget: '_blank',
