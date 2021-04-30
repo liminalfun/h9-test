f192839d71c21e7a4a0ad34d67a1926578ba4b75
liminalfun <xelanawotka@gmail.com>
2021-04-30 19:47:32 -0400

    Update vue layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,16 +1,10 @@
 <template>
-  <div class="layout">
+  
+  <div>
     <Nav/>
-    <!-- <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header> -->
-    <slot/>
+    <div class="layout">
+      <slot/>
+    </div>
   </div>
 </template>
 
@@ -40,21 +34,7 @@ body {
 }
 
 .layout {
-  max-width: 760px;
+  max-width: 100vw;
   margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
 }
 </style>
